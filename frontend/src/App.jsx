e0a45d68f26302c34d65f17a2fa077eea6ff1b33
Ella Ye <yeella905@gmail.com>
2025-06-05 21:32:02 -0400

    new:tested out Tailwind features and libraries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,33 +2,84 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import {
+  IconArrowLeft,
+  IconSettings,
+  IconUserBolt,
+  IconList
+} from "@tabler/icons-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+   
+  const links = [
+    {
+      label: "Listings",
+      href: "#",
+      icon: (
+        <IconList className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Profile",
+      href: "#",
+      icon: (
+        <IconUserBolt className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Settings",
+      href: "#",
+      icon: (
+        <IconSettings className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Logout",
+      href: "#",
+      icon: (
+        <IconArrowLeft className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex min-h-screen flex-row">
+        {/* Sidebar */}
+          <Sidebar open={open} setOpen={setOpen} animate={true} >
+                <SidebarBody className="justify-between gap-10 min-h-screen">
+                  <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+                   
+                    <div className="mt-8 flex flex-col gap-2">
+                      {links.map((link, idx) => (
+                        <SidebarLink key={idx} link={link} />
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <SidebarLink
+                      link={{
+                        label: "Profile",
+                        href: "#",
+                        icon: (
+                          <img
+                            src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png"
+                            className="h-7 w-7 shrink-0 rounded-full"
+                            width={50}
+                            height={50}
+                            alt="Avatar" />
+                        ),
+                      }} />
+                  </div>
+                </SidebarBody>
+              </Sidebar>
+        {/* End Sidebar */}
+
+        {/* Add all other UI components here */}
+
+             <p>tester</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
