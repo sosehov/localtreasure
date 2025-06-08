be938a30a5f19d5678357556059fb6c06ca92df4
Soseh <sosehv@gmail.com>
2025-06-07 21:52:45 -0400

    Moves routing logic from main.jsx to App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,38 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeRoute from './routes/home/index.jsx';
-import ProfileRoute from './routes/profile/index.jsx';
-import MainLayout from './MainLayout.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.jsx';
-import AuthPage from './components/AuthPage.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          {/* Route without layout for auth page */}
-          <Route path="/auth" element={<AuthPage/>} />
-          
-          {/* Routes with layout */}
-          <Route path="/" element={
-            <MainLayout>
-              <HomeRoute/>
-            </MainLayout>
-          } />
-          
-          <Route path="/profile" element={
-            <MainLayout>
-              <ProtectedRoute>
-                <ProfileRoute/>
-              </ProtectedRoute>
-            </MainLayout>
-          } />
-        </Routes>
+        <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
