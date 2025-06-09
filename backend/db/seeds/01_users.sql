71f15a71ea5709b4b6b50e48312dea0ff1419d39
CodeJuuun <Chomseng.a@gmail.com>
2025-06-09 01:32:05 -0400

    inserted values into user file

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, address, bio, is_admin, contact_info ) 
+VALUES (
+  'Alice Henderson',
+  'AliceH@gmail.dom',
+  '54 Sandflower Street, Oakland ON A1B CD2',
+  'I am an avid gardener, tending to my flowerbeds and growing different kinds of vegatables! I also enjoy watching soap operas, Days of Our Lives is my favourite!',
+  true,
+  '123-654-789'
+  );
+
