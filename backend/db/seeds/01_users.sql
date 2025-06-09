e3d6b764600922bec3867d3925fb054d1ddac6b0
Soseh <sosehv@gmail.com>
2025-06-09 12:55:38 -0400

    Adds comma between password and bio in both INSERT statements

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,20 +1,21 @@
-INSERT INTO users (name, email, address, password, bio, is_admin, contact_info ) 
+INSERT INTO users (name, email, address, password, bio, is_admin, contact_info)
 VALUES (
   'Alice Henderson',
   'AliceH@gmail.dom',
   '54 Sandflower Street, Oakland ON A1B CD2',
-  'ilovetogarden123'
+  'ilovetogarden123',
   'I am an avid gardener, tending to my flowerbeds and growing different kinds of vegatables! I also enjoy watching soap operas, Days of Our Lives is my favourite!',
   true,
   '123-654-7890'
-  );
+);
 
 INSERT INTO users (name, email, address, password, bio, is_admin, contact_info)
 VALUES (
   'Michael Lee',
   'LeeM@gmail.com',
   '123 granville island, 14 Avenue BC V6K 2P5',
-  'traveler5000'
+  'traveler5000',
   'I love to travel to different countries around the world! I also enjoy doing hikes',
-  true, '789-635-1471'
+  true,
+  '789-635-1471'
 );
