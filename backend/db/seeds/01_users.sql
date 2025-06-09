71ee586246382918478c83da39db4b00e5238ac4
CodeJuuun <Chomseng.a@gmail.com>
2025-06-09 03:14:44 -0400

    added seeding value

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -5,6 +5,14 @@ VALUES (
   '54 Sandflower Street, Oakland ON A1B CD2',
   'I am an avid gardener, tending to my flowerbeds and growing different kinds of vegatables! I also enjoy watching soap operas, Days of Our Lives is my favourite!',
   true,
-  '123-654-789'
+  '123-654-7890'
   );
 
+INSERT INTO users (name, email, address, bio, is_admin, contact_info)
+VALUES (
+  'Michael Lee',
+  'LeeM@gmail.com',
+  '123 granville island, 14 Avenue BC V6K 2P5',
+  'I love to travel to different countries around the world! I also enjoy doing hikes',
+  true, '789-635-1471'
+);
