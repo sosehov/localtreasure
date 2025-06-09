aaf3b9b71ce97db1d30cb345574f23ee2fc7ba4a
CodeJuuun <Chomseng.a@gmail.com>
2025-06-09 01:42:38 -0400

    added sales table and columns

---
+++ backend/db/schema/03_sales.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS events CASCADE;
+
+CREATE TABLE sales (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title TEXT NOT NULL,
+  description VARCHAR(255),
+  price_cents, INTEGER NOT NULL,
+  category_id INTEGER REFERENCES categories(id),
+  is_sold BOOLEAN DEFAULT FALSE,
+  photo_url VARCHAR(255),
+)
