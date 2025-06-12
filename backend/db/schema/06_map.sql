58143e751242cbd843477059584a94dc82da09ce
CodeJuuun <Chomseng.a@gmail.com>
2025-06-12 15:20:23 -0400

    entered map queries

--- backend/db/schema/06_map.sql
+++ backend/db/schema/06_map.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS map CASCADE;
 
 CREATE TABLE map (
-  
+  id SERIAL PRIMARY KEY NOT NULL,
+  sale_id INT REFERENCES sales(id),
+  event_id INT REFERENCES events(event_id),
+  location GEOGRAPHY(POINT) NOT NULL,
+  address TEXT,
 )
