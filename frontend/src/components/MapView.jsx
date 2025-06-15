1e5b1184df049ad4a5433e88a7f9e9156136dfa8
CodeJuuun <Chomseng.a@gmail.com>
2025-06-15 17:23:15 -0400

    set locations hook wtih empty array that will contain coordinates

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -2,5 +2,12 @@ import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function MapView() {
-  
+  const [locations, setLocations] = useState([]) // will contain the coorindate numbers
+
+  useEffect(() => {
+    fetch('')
+    .then(res => res.json())
+    .then(setLocations)
+    .catch(err => console.error('error, did not fetch', err));
+  }, []);
 }
