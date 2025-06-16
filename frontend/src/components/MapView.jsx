22963ead417480921ef8fb2c5a04f6cdf504ef02
CodeJuuun <Chomseng.a@gmail.com>
2025-06-16 01:50:26 -0400

    fixed } which made the code outside the functoin

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -10,36 +10,36 @@ function MapView() {
       .then(setLocations)
       .catch(err => console.error('error, did not fetch', err));
   }, []);
+
+  return (
+    <div style={{ height: '500px', width: '100%' }}>
+      <MapContainer
+        center={[43.65, -79.38]} // this should be Toronto,currently hardcoded coordinates
+        zoom={4}
+        scrollWheelZoom={false}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {locations.map((pos) => {
+          const [lng, lat] = pos.locations.coordinates;
+          return (
+            <Marker key={pos.id} position={[lat, lng]}>
+              <Popup>
+                <div>
+                  <strong>Address:</strong><br />
+                  {pos.address}
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </div>
+  );
 }
-
-return (
-  <div style={{ height: '500px', width: '100%' }}>
-    <MapContainer
-      center={[43.65, -79.38]} // this should be Toronto,currently hardcoded coordinates
-      zoom={4}
-      scrollWheelZoom={false}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      {locations.map((pos) => {
-        const [lng, lat] = pos.locations.coordinates;
-        return (
-          <Marker key={pos.id} position={[lat, lng]}>
-            <Popup>
-              <div>
-                <strong>Address:</strong><br />
-                {loc.address}
-              </div>
-            </Popup>
-          </Marker>
-        );
-      })}
-    </MapContainer>
-  </div>
-);
 
 export default MapView;
