f3d46a0925ffbfa65ec4ab6ba175c5df5f511813
CodeJuuun <Chomseng.a@gmail.com>
2025-06-16 01:42:34 -0400

    completed mapView component

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -2,30 +2,44 @@ import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function MapView() {
-  const [locations, setLocations] = useState([]) // will contain the coorindate numbers
+  const [locations, setLocations] = useState([]); // will contain the coorindate numbers
 
   useEffect(() => {
     fetch('/api/locations') //temporary route
-    .then(res => res.json())
-    .then(setLocations)
-    .catch(err => console.error('error, did not fetch', err));
+      .then(res => res.json())
+      .then(setLocations)
+      .catch(err => console.error('error, did not fetch', err));
   }, []);
 }
 
 return (
   <div style={{ height: '500px', width: '100%' }}>
-      <MapContainer
+    <MapContainer
       center={[43.65, -79.38]} // this should be Toronto,currently hardcoded coordinates
       zoom={4}
       scrollWheelZoom={false}
-      style={{height: '100%', width: '100%'}}
-      >
-        <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {locations.map((pos) => {
+        const [lng, lat] = pos.locations.coordinates;
+        return (
+          <Marker key={pos.id} position={[lat, lng]}>
+            <Popup>
+              <div>
+                <strong>Address:</strong><br />
+                {loc.address}
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
   </div>
-)
+);
 
 export default MapView;
