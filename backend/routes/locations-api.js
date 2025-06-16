5436b69e034fe1b4eaf67ab4b8e97c87a913a097
CodeJuuun <Chomseng.a@gmail.com>
2025-06-16 00:51:29 -0400

    added getter route to fetch api locations

--- backend/routes/locations-api.js
+++ backend/routes/locations-api.js
@@ -1,4 +1,19 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 const db = require('../db');
 
+//--------------------------------------------------------------
+router.get('/', async (req, res) => {
+  try {
+    const result = await db.query(`
+      SELECT id, sale_id, event_id, ST_AsGeoJSON(location):: json AS location, address 
+      FROM map;
+      `);
+      res.json(result.rows);
+  } catch (err) {
+    console.error('error in fetching locations:', err);
+    res.status(500).json({ error: 'internal server error'})
+  }
+});
+
+//--------------------------------------------------------------
