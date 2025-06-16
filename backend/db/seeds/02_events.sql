d72d8b5193ee531578a0ecd07dbf8f0daa6a6262
Soseh <sosehv@gmail.com>
2025-06-16 13:20:52 -0400

    Updates all_tables schema file to match the events schema and updates the seed file

--- backend/db/seeds/02_events.sql
+++ backend/db/seeds/02_events.sql
@@ -1,15 +1,14 @@
-INSERT INTO events (user_id, title, description, address, start_time, end_time, DATE, is_active, category_id, location)
+INSERT INTO events (user_id, title, description, address, start_time, end_time, DATE, is_active, category_id)
 VALUES (
   1,
   'Spring Cleaning Sale!',
   'Useful items that will assist in things such as trimming your lawn or cleaning your car!',
   '123 Mapletree Lane',
-  '2025-06-15 10:00:00',
-  '2025-06-15 14:00:00',
-  DEFAULT,
+  '10:00:00',
+  '14:00:00',
+  '2025-06-15',
   TRUE,
-  1,
-  ST_MakePoint(-80.3200, 43.0250)::GEOGRAPHY
+  1
 ),
 
 (
@@ -19,8 +18,7 @@ VALUES (
   'Granville Island Park, Vancouver, BC',
   '09:00:00',
   '13:00:00',
-  DEFAULT,
+  '2025-06-16',
   TRUE,
-  2,
-  ST_MakePoint(-123.1359, 49.2702)::GEOGRAPHY -- granville island
+  2
 );
