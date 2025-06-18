469588b03579714a65576dc7319f00e951828c97
CodeJuuun <Chomseng.a@gmail.com>
2025-06-18 16:14:31 -0400

    removed leaflet.css import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import App from './App.jsx';
-import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
