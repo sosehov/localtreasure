3f7b5549cf3902db7a02465de5f9928082eeab10
Anika Nawar <nawarani47@gmail.com>
2025-06-19 01:32:59 -0400

    had to manually add back messagerooms comp

---
+++ frontend/src/components/messages/MessageRooms.jsx
@@ -0,0 +1,59 @@
+import { useAuth } from "../../contexts/AuthContext";
+import { useState, useEffect } from 'react';
+import ConversationCard from './ConversationCard';
+
+
+const URL = 'http://localhost:8080';
+
+const MessageRooms = () => {
+  const { token, makeAuthenticatedRequest, user } = useAuth();
+  
+  // state
+  const [rooms, setRooms] = useState([]);
+  
+  // fetch rooms
+  useEffect(() => {
+    // dont run if user or token hasn't loaded in yet
+    if (!user || !token) return;
+
+    // get all the rooms for this user from db once when the page loads.
+    const fetchRooms = async () => {
+      try {
+        const fetchURL = `api/messageRooms?senderId=${user.id}`
+        const response = await makeAuthenticatedRequest(fetchURL, {
+          method: "GET"
+        });
+        const data = await response.json();
+        setRooms(data.rooms);
+        console.log('rooms after fetch:', rooms);
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+      }
+    };
+    
+    fetchRooms();
+
+  }, [ user, token, makeAuthenticatedRequest]);
+
+
+  return (
+    <div>
+    {rooms.length > 0 && user.name ? (
+      <>
+        <p>{user.name}'s Messages</p>
+        <ul>
+          {rooms.map((room) => (
+            <li key={room.id}>
+              <ConversationCard room={room} />
+            </li>
+          ))}
+        </ul>
+      </>
+      ) : (
+        <p>Loading rooms...</p>
+      )}
+    </div>
+  )
+};
+
+export default MessageRooms;
