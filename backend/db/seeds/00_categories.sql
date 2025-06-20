838ffede4b266535177d11cc39a12ec293e7e79c
CodeJuuun <Chomseng.a@gmail.com>
2025-06-20 01:11:44 -0400

    seeded categories

--- backend/db/seeds/00_categories.sql
+++ backend/db/seeds/00_categories.sql
@@ -2,3 +2,6 @@ INSERT INTO categories (name, description)
 VALUES 
 ('garden', 'Anything related to your garden, lawns, tools'),
 ('outdoors', 'Camping, hiking, biking, anything outside!');
+('tech', 'Gadgets, cables, old phones, and anything tech-related'),
+('books', 'Novels, textbooks, magazines, and educational materials'),
+('kids', 'Toys, clothes, and gear for babies and children'),
