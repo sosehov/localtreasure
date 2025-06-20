ca359a53236777a428fa4c5a7ae3b9fefb638610
CodeJuuun <Chomseng.a@gmail.com>
2025-06-20 01:32:43 -0400

    seeded items for sale

--- backend/db/seeds/03_sales.sql
+++ backend/db/seeds/03_sales.sql
@@ -2,3 +2,4 @@ INSERT INTO sales (user_id, title, description, price_cents, category_id, is_sol
 VALUES 
 (1, 'Weed Wacker', 'Only used a few times, stil in great condition, getting a lawn mower instead!', 8000, 1, FALSE, 'https://images.pexels.com/photos/32421761/pexels-photo-32421761.png'),
 (2, 'Hiking Backpack', 'Lightly used hiking backpack, perfect for day trips or camping adventures.', 4500, 2, FALSE, 'https://images.pexels.com/photos/18357715/pexels-photo-18357715.jpeg');
+(3, 'Harry Potter Box Set', 'All 7 books in good condition. All hardcover.', 6500, 3, FALSE, 'https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg' )
