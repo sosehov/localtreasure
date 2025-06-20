6f73cfa3430b654b53a1cb85f486fb8a0dc8fd47
CodeJuuun <Chomseng.a@gmail.com>
2025-06-20 01:45:47 -0400

    fixed comma

--- backend/db/seeds/00_categories.sql
+++ backend/db/seeds/00_categories.sql
@@ -1,5 +1,5 @@
 INSERT INTO categories (name, description)
 VALUES 
 ('garden', 'Anything related to your garden, lawns, tools'),
-('outdoors', 'Camping, hiking, biking, anything outside!');
-('books', 'Novels, textbooks, magazines, and educational materials'),
+('outdoors', 'Camping, hiking, biking, anything outside!'),
+('books', 'Novels, textbooks, magazines, and educational materials');
