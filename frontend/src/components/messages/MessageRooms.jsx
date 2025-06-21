3eb9c739aa44585eeb5acd408f96fac5513753c0
Anika Nawar <nawarani47@gmail.com>
2025-06-21 03:16:09 -0400

    isolated messageRooms hooks

--- frontend/src/components/messages/MessageRooms.jsx
+++ frontend/src/components/messages/MessageRooms.jsx
@@ -1,40 +1,12 @@
-import { useAuth } from "../../contexts/AuthContext";
-import { useState, useEffect } from 'react';
+import useMessageRooms from '../../hooks/useMessageRooms';
+'hooks/useMessageRooms';
 import ConversationCard from './ConversationCard';
 
 
 const URL = 'http://localhost:8080';
 
 const MessageRooms = () => {
-  const { token, makeAuthenticatedRequest, user } = useAuth();
-  
-  // state
-  const [rooms, setRooms] = useState([]);
-  
-  // fetch rooms
-  useEffect(() => {
-    // dont run if user or token hasn't loaded in yet
-    if (!user || !token) return;
-
-    // get all the rooms for this user from db once when the page loads.
-    const fetchRooms = async () => {
-      try {
-        const fetchURL = `api/messageRooms?senderId=${user.id}`
-        const response = await makeAuthenticatedRequest(fetchURL, {
-          method: "GET"
-        });
-        const data = await response.json();
-        setRooms(data.rooms);
-        console.log('rooms after fetch:', rooms);
-      } catch (error) {
-        console.error("Error fetching rooms:", error);
-      }
-    };
-    
-    fetchRooms();
-
-  }, [ user, token, makeAuthenticatedRequest]);
-
+  const { rooms, user } = useMessageRooms();
 
   return (
     <div>
